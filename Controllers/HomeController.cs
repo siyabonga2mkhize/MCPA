5bb25746db59f22b7fead9457de254f3e10f31b6
Siyabonga Mkhize <siyabonga2mkhize@gmail.com>
2025-04-02 11:23:23 +0200

    added all the files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using MCPAProject.Models;
+using System.Web.Mvc;
+
+namespace MCPAProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Student_info()
+        {
+            return View();
+        }
+
+        public ActionResult RedirectPage(studName model)
+        {
+            if (model.PojNames == Names.Siyabonga_Mkhize)
+            {
+                return Redirect("\\Student Profile\\Siyabonga Mkhize\\Siyabonga.html");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ Models/studName.cs
@@ -0,0 +1,16 @@
+﻿namespace MCPAProject.Models
+{
+    public class studName
+    {
+        public Names PojNames { get; set; }
+
+    }
+    public enum Names
+    {
+        Thulani_Thabethe = 1,
+        Siyamthanda_Dlamini = 2,
+        Jack_Dickoko = 3,
+        Siyabonga_Mkhize = 4,
+        Ayanda_Mthembu = 5
+    }
+}
